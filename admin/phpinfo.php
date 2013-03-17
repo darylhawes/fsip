2680194aa1cc0aa6a27553006effbbe195904e92
Daryl Hawes <git@sdelargy.com>
2013-03-17 22:58:19 +0000

    Created new phpinfo file
    Created new phpinfo file

---
+++ admin/phpinfo.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+// FSIP based on Alkaline
+// Alkaline Copyright (c) 2010-2012 by Budin Ltd. Released to the wild under MIT license.
+// http://www.alkalineapp.com/
+*/
+
+require_once('../config.php');
+require_once(PATH . CLASSES . 'fsip.php');
+
+$fsip = new FSIP;
+$user = new User;
+
+$user->perm(true);
+
+require_once(PATH . INCLUDES . '/admin_header.php');
+phpinfo();
+require_once(PATH . INCLUDES . '/admin_footer.php');
+
+?>
