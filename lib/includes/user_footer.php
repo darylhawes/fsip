f3dbe8bfa92a54145d3f669e7e4f8609034f39bc
Daryl Hawes <git@sdelargy.com>
2013-03-29 18:37:49 +0000

    Added debugger info
    Added debugger info

--- lib/includes/user_footer.php
+++ lib/includes/user_footer.php
@@ -4,13 +4,12 @@
 			Powered by <a href="http://github.com/darylhawes/fsip">FSIP</a> based on <a href="http://www.alkalineapp.com/">Alkaline</a> under MIT license.
 			<?php 
 			
-			if(!empty($fsip)){
-				if($fsip->returnConf('maint_debug')){
-					$debug = $fsip->debug();
-					echo 'Execution time: ' . round($debug['execution_time'], 3) . ' seconds. Queries: ' . $debug['queries']  . '. ';
-				}
-				
-				echo FSIP::returnErrors();
+			$debugger = getDebugger();
+			echo $debugger->returnErrors();
+
+			if (returnConf('maint_debug')) {
+				$debug = $debugger->debug();
+				echo 'Execution time: ' . round($debug['execution_time'], 3) . ' seconds. Queries: ' . $debug['queries']  . '. ';
 			}
 			
 			echo Orbit::promptTasks();
