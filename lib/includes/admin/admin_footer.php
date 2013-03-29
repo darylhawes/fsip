358beb92f60857dabe87e75193ef04cc5af61172
Daryl Hawes <git@sdelargy.com>
2013-03-29 18:33:04 +0000

    Admin header and footer moved into new admin subfolder of includes
    Admin header and footer moved into new admin subfolder of includes

---
+++ lib/includes/admin/admin_footer.php
@@ -0,0 +1,20 @@
+		</div>
+		<hr class="invisible" />
+		<div id="footer" class="span-24 last">
+			Powered by <a href="http://github.com/darylhawes/fsip">FSIP</a> based on <a href="http://www.alkalineapp.com/">Alkaline</a> under MIT license.
+			<?php 
+		
+			$debugger = getDebugger();
+			echo $debugger->returnErrors();
+
+			if (returnConf('maint_debug')) {
+				echo = $debugger->getDebugString().'<br />';
+				print_r($_SESSION['fsip']['debug']);
+			}
+			
+			echo Orbit::promptTasks();
+			?>
+		</div>
+	</div>
+	</body>
+</html>
