df6d32178e8913c9b2c760cbb51cc588b9737a88
Daryl Hawes <git@sdelargy.com>
2013-04-05 18:59:33 +0100

    Slowly dismantling old guest access capabilities to be replaced
    Slowly dismantling old guest access capabilities to be replaced

--- access.php
+++ access.php
@@ -5,7 +5,7 @@
 // Alkaline Copyright (c) 2010-2012 by Budin Ltd. Released to the wild under MIT license.
 // http://www.alkalineapp.com/
 */
-
+/*
 require_once('config.php');
 $user = new User();
 
@@ -13,6 +13,11 @@ $user->access($_REQUEST['id']);
 
 session_write_close();
 
+$location = LOCATION . BASE;
+headerLocationRedirect($location);
+exit();
+*/
+require_once('config.php');
 $location = LOCATION . BASE;
 headerLocationRedirect($location);
 exit();
