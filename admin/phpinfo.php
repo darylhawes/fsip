cb5dbd30d46437b0a16954e43b0434d39cf6fa45
Daryl Hawes <git@sdelargy.com>
2013-08-08 00:54:16 +0100

    phpinfo cleanup
    phpinfo cleanup

--- admin/phpinfo.php
+++ admin/phpinfo.php
@@ -13,7 +13,25 @@ $user->hasPermission('admin', true);
 
 require_once(PATH . INCLUDES . '/admin_header.php');
 
-phpinfo();
+echo '<div class="phpinfo">';
+
+ob_start();
+phpinfo(INFO_GENERAL + INFO_CONFIGURATION + INFO_MODULES);
+$html = ob_get_contents();
+ob_end_clean();
+
+/// Delete styles from output
+$html = preg_replace('#(\n?<style[^>]*?>.*?</style[^>]*?>)|(\n?<style[^>]*?/>)#is', '', $html);
+$html = preg_replace('#(\n?<head[^>]*?>.*?</head[^>]*?>)|(\n?<head[^>]*?/>)#is', '', $html);
+/// Delete DOCTYPE from output
+$html = preg_replace('/<!DOCTYPE html PUBLIC.*?>/is', '', $html);
+/// Delete body and html tags
+$html = preg_replace('/<html.*?>.*?<body.*?>/is', '', $html);
+$html = preg_replace('/<\/body><\/html>/is', '', $html);
+
+echo $html;
+
+echo '</div>';
 
 require_once(PATH . INCLUDES . '/admin_footer.php');
 
