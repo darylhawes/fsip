8b00b9a2eff15773ab74af2afd64342fad2bb7ad
Daryl Hawes <git@sdelargy.com>
2013-08-19 00:30:17 +0100

    phpdoc updates
    phpdoc updates

--- admin/comments.php
+++ admin/comments.php
@@ -1,10 +1,16 @@
 <?php
 
-/*
-// FSIP based on Alkaline
-// Alkaline Copyright (c) 2010-2012 by Budin Ltd. Released to the wild under MIT license.
-// http://www.alkalineapp.com/
-*/
+/**
+ * FSIP based on Alkaline
+ * 
+ *
+ * http://www.alkalineapp.com/
+ * Alkaline Copyright (c) 2010-2012 by Budin Ltd. Released to the wild under MIT license.
+ *
+ * @package FSIP
+ * @subpackage admin
+ * @since 1.2
+ */
 
 require_once('../config.php');
 //echo "comments 1<br />";
--- admin/help.php
+++ admin/help.php
@@ -1,5 +1,17 @@
 <?php
 
+/**
+ * FSIP based on Alkaline
+ * 
+ *
+ * http://www.alkalineapp.com/
+ * Alkaline Copyright (c) 2010-2012 by Budin Ltd. Released to the wild under MIT license.
+ *
+ * @package FSIP
+ * @subpackage admin
+ * @since 1.2
+ */
+ 
 require_once('../config.php');
 require_once(PATH. EXTENSIONS .'markdown/functions/markdown.php');
 require_once(PATH. EXTENSIONS .'markdown/functions/smartypants.php');
